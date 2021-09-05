b82da80cf6f1ff4fff97449a85985800a9626681
4llower <i.4llower@gmail.com>
2021-09-05 21:09:51 +0300

    add: provider deps & base models

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:open_scooter_ui/models/balance.dart';
+import 'package:open_scooter_ui/models/scooter.dart';
+import 'package:open_scooter_ui/models/user.dart';
 import 'package:open_scooter_ui/screens/balance.dart';
 import 'package:open_scooter_ui/screens/finish.dart';
 import 'package:open_scooter_ui/screens/home.dart';
 import 'package:open_scooter_ui/screens/login.dart';
 import 'package:open_scooter_ui/screens/qr.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(App());
+}
+
+class Root extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        Provider<BalanceModel>(create: (_) => BalanceModel()),
+        Provider<UserModel>(create: (_) => UserModel()),
+        Provider<ScooterModel>(create: (_) => ScooterModel())
+      ],
+      child: App(),
+    );
+  }
 }
 
 class App extends StatelessWidget {
