655462a96c8287bafdd48a40a92d741e9030defd
4llower <i.4llower@gmail.com>
2021-09-26 18:19:40 +0300

    add: auth widges raw

---
+++ lib/feature/presentation/pages/auth_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:open_scooter_ui/feature/presentation/widgets/forms/enter_phone.dart';
+
+class AuthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: EnterPhone(),
+      ),
+      appBar: AppBar(title: Center(child: Text("Open Scooter"))),
+    );
+  }
+}
