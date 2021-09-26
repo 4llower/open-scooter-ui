6dda6e9be98c1644310abcd15ff0289bc2d60fd3
4llower <i.4llower@gmail.com>
2021-09-26 16:19:18 +0300

    add: entites

---
+++ lib/feature/domain/entities/user_entity.dart
@@ -0,0 +1,11 @@
+import 'package:open_scooter_ui/feature/domain/entities/balance_entity.dart';
+import 'package:open_scooter_ui/feature/domain/entities/location_entity.dart';
+
+class UserEntity {
+  final String phone;
+  final LocationEntity location;
+  final BalanceEntity balance;
+
+  UserEntity(
+      {required this.phone, required this.location, required this.balance});
+}
