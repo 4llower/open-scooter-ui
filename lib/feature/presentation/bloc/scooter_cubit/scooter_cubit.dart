a1cb24f6c9c89cba71ed182aac32cfcd55578729
4llower <i.4llower@gmail.com>
2021-11-29 01:18:59 +0300

    Add custom info window

--- lib/feature/presentation/bloc/scooter_cubit/scooter_cubit.dart
+++ lib/feature/presentation/bloc/scooter_cubit/scooter_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -9,6 +10,7 @@ import 'package:open_scooter_ui/feature/presentation/bloc/scooter_cubit/scooter_
 class ScooterCubit extends Cubit<ScooterState> {
   final GetAllScooters getAllScooters;
   GoogleMapController? controller;
+  CustomInfoWindowController? _customInfoWindowController;
 
   Location _location = Location();
 
@@ -22,21 +24,25 @@ class ScooterCubit extends Cubit<ScooterState> {
     emit(ScooterLoading());
 
     final failureOrScooter = await getAllScooters(GetAllScooterParams());
-    BitmapDescriptor scooterIcon = await BitmapDescriptor.fromAssetImage(ImageConfiguration(size: Size(128.0, 128.0)), 'assets/icons/scooter.png');
+    BitmapDescriptor scooterIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(128.0, 128.0)),
+        'assets/icons/scooter.png');
 
     failureOrScooter.fold(
         (_) =>
             throw UnimplementedError('[Failure] ScooterCubit getAllScooters'),
         (resp) => {
-          resp.forEach((element) {
-            this._add(element, scooterIcon);
-          }),
-          emit(ScooterLoaded(scooterList: resp, markers: this.markers))
-        });
+              resp.forEach((element) {
+                this._add(element, scooterIcon);
+              }),
+              emit(ScooterLoaded(scooterList: resp, markers: this.markers))
+            });
   }
 
-  void onMapCreated(GoogleMapController controller) {
+  void onMapCreated(GoogleMapController controller,
+      CustomInfoWindowController customInfoWindowController) {
     this.controller = controller;
+    this._customInfoWindowController = customInfoWindowController;
     var subscription = this._location.onLocationChanged.listen((l) {});
 
     subscription = this._location.onLocationChanged.listen((l) {
@@ -53,15 +59,73 @@ class ScooterCubit extends Cubit<ScooterState> {
   }
 
   void _add(ScooterEntity scooter, BitmapDescriptor icon) {
-
     final MarkerId markerId = MarkerId('marker_id_' + scooter.id.toString());
 
     final Marker marker = Marker(
-      markerId: markerId,
-      icon: icon,
-      position: LatLng(scooter.location.lat, scooter.location.lng),
-      infoWindow: InfoWindow(title: scooter.id.toString(), snippet: '*'),
-    );
+        markerId: markerId,
+        icon: icon,
+        position: LatLng(scooter.location.lat, scooter.location.lng),
+        onTap: () {
+          _customInfoWindowController?.addInfoWindow!(
+            Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black54,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.electric_scooter,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Flexible(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Scooter #' + scooter.id.toString(),
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(
+                                height: 6.0,
+                              ),
+                              Text(scooter.chargeLevel.toString() + '%🔋', style: TextStyle(color: Colors.greenAccent)),
+                              // TODO: Add unit
+                              SizedBox(
+                                height: 7.0,
+                              ),
+                              Text(
+                                "Unlock " +
+                                    scooter.cost.unlock.toString() +
+                                    'Br',
+                              ),
+                              Text("Minute " +
+                                  scooter.cost.minute.toString() +
+                                  'Br'),
+                            ],
+                          ))
+                        ],
+                      ),
+                    ),
+                    width: double.infinity,
+                    height: 300,
+                  ),
+                ),
+              ],
+            ),
+            LatLng(scooter.location.lat, scooter.location.lng),
+          );
+        });
 
     this.markers.add(marker);
   }
--- lib/feature/presentation/widgets/map_widget.dart
+++ lib/feature/presentation/widgets/map_widget.dart
@@ -1,3 +1,4 @@
+import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,11 +6,24 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:open_scooter_ui/feature/presentation/bloc/scooter_cubit/scooter_cubit.dart';
 import 'package:open_scooter_ui/feature/presentation/bloc/scooter_cubit/scooter_state.dart';
 
+class MapWidget extends StatefulWidget {
+  @override
+  _MapWidgetState createState() => _MapWidgetState();
+}
 
-class MapWidget extends StatelessWidget {
+class _MapWidgetState extends State<MapWidget> {
   // TODO: Add dotenv camera location and default zoom if not access
   static LatLng _initialCameraPosition = LatLng(50.893009, 25.567444);
   static double _defaultZoom = 20;
+
+  CustomInfoWindowController _customInfoWindowController =
+      CustomInfoWindowController();
+
+  @override
+  void dispose() {
+    _customInfoWindowController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,17 +33,33 @@ class MapWidget extends StatelessWidget {
       }
 
       if (state is ScooterLoading) {
-        return Center(child: CircularProgressIndicator());;
+        return Center(child: CircularProgressIndicator());
+        ;
       }
 
       if (state is ScooterLoaded) {
         // return Text(state.scooterList[0].chargeLevel.toString());
-        return GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(target: _initialCameraPosition, zoom: _defaultZoom),
-          onMapCreated: (ctrl) => _onMapCreated(context, ctrl),
-          markers: state.markers,
-        );
+        return Stack(children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: CameraPosition(
+                target: _initialCameraPosition, zoom: _defaultZoom),
+            onMapCreated: (ctrl) => _onMapCreated(context, ctrl),
+            markers: state.markers,
+            onTap: (position) {
+              _customInfoWindowController.hideInfoWindow!();
+            },
+            onCameraMove: (position) {
+              _customInfoWindowController.onCameraMove!();
+            },
+          ),
+          CustomInfoWindow(
+            controller: _customInfoWindowController,
+            height: 96,
+            width: 170,
+            offset: 50,
+          ),
+        ]);
       }
 
       return Text('Unexpected error');
@@ -37,6 +67,7 @@ class MapWidget extends StatelessWidget {
   }
 
   _onMapCreated(BuildContext context, GoogleMapController ctrl) {
-    BlocProvider.of<ScooterCubit>(context)..onMapCreated(ctrl);
+    _customInfoWindowController.googleMapController = ctrl;
+    BlocProvider.of<ScooterCubit>(context)..onMapCreated(ctrl, _customInfoWindowController);
   }
 }
